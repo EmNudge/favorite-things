7a2afae57128c2266d49ef540cbf8a295b3b5390
EmNudge <24513691+EmNudge@users.noreply.github.com>
2023-11-26 00:38:08 +0800

    add font and new product (real)

--- src/data.ts
+++ src/data.ts
@@ -394,6 +394,22 @@ export const favoriteThings = Object.entries(
             Darn Tough is a pretty good brand for these as they use a decently high percentage of merino wool in their products, but they're not the only company in this space. $20 may feel like a lot for a single pair of socks, but it's worth it!
         `,
       },
+      {
+        href: "https://www.muji.com/sg/products/cmdty/detail/4550182603033",
+        title: "Dotted Notebook",
+        description: "A notebook for all kinds of things",
+        image: 'dotted-notebook.webp',
+        points: [
+          'dots',
+          'for writing in real life',
+          'feels like paper (because it is)'
+        ],
+        pitch: `
+          It's the perfect compromise between a lined notebook and graph paper. A lined notebook prevents you from sketching vertically through the lines and graph paper is often too noisy for regular writing. A dotted notebook lets you follow the loose guides for writing, but also allows larger sketches that are optionally aligned on a grid.
+
+          I prefer a much lighter dotted notebook if possible, which is something difficult to tell in person. It should be light enough to not be visible from afar. You don't want the dots distracting from the actual writing.
+        `
+      }
     ],
     "Personal Care Tools": [
       {
