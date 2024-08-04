d1c22d28a1242652545e1d75e1c9dfd0bfd5e164
EmNudge <24513691+EmNudge@users.noreply.github.com>
2024-08-03 19:31:08 -0700

    move to external API

--- src/data.ts
+++ src/data.ts
@@ -1,7 +1,3 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import matter from 'gray-matter';
-
 export interface FavoriteItem {
   href: string;
   title: string;
@@ -13,29 +9,25 @@ export interface FavoriteItem {
 }
 
 const categoryOrdering = Object.fromEntries([...[
-  'Every Day Carry',
-  'Travel',
-  'Kitchen Tools',
-  'Tech',
-  'Personal Care Tools',
-  'Programming Languages',
-  'HTML Colors',
+  "Every Day Carry",
+  "Travel",
+  "Kitchen Tools",
+  "Tech",
+  "Personal Care Tools",
+  "Programming Languages",
+  "HTML Colors",
 ].entries()].map(([index, key]) => [key, index]));
 
-const contentDir = './src/content';
-export const favoriteThings = fs.readdirSync(contentDir).map(folder => {
-  const folderPath = path.join(contentDir, folder);
-  
-  const items = fs.readdirSync(folderPath).map(file => {
-    const fileContent = fs.readFileSync(path.join(folderPath, file), 'utf-8');
-    const { content, data } = matter(fileContent);
+export const favoriteThings = fetch(import.meta.env.API_URL)
+  .then((res) =>
+    res.json() as unknown as { folder: string; items: FavoriteItem[] }[]
+  )
+  .then((data) =>
+    data.sort((a, b) => {
+      return categoryOrdering[a.folder] - categoryOrdering[b.folder];
+    })
+  );
 
-    const slug = file.slice(0, -3);
-    return { pitch: content, slug, ...(data as any) } as FavoriteItem;
-  });
-
-  return { folder, items };
-}).sort((a, b) => categoryOrdering[a.folder] - categoryOrdering[b.folder]);
-
-
-export const allThings = favoriteThings.map(section => section.items).flat();
+export const allThingsPromise = favoriteThings.then((things) =>
+  things.map((section) => section.items).flat()
+);
